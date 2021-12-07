6e5c74f94fc6a0a1b243fb6225ee9b851092f1a8
Katie Masiello <ka.masiello@gmail.com>
2021-12-07 10:49:18 -0800

    clean up

--- pin_to_local.r
+++ pin_to_local.r
@@ -2,8 +2,8 @@ library(pins)
 #local board
 board1 <- board_local()
 board1
-# Pin board <pins_board_folder>
-# Path: '~/Library/Application Support/pins'
+
+
 
 #write some pins
 board1 %>% pin_write(mtcars)
@@ -12,15 +12,14 @@ board1 %>% pin_write(palmerpenguins::penguins, name = "penguins", versioned = TR
 
 #what do we have?
 board1 %>% pin_list()
-# [1] "faithful" "mtcars"   "penguins"
+
+
+#search
+board1 %>% pin_search("cars")
 
 #read the pin
 board1 %>% pin_read("mtcars")
 
 #list versions
 board1 %>% pin_versions("penguins")
-# A tibble: 2 × 3
-#   version                created             hash
-#   <chr>                  <dttm>              <chr>
-# 1 20211207T005403Z-84255 2021-12-06 16:54:00 84255
-# 2 20211207T005855Z-84255 2021-12-06 16:58:00 84255
+
