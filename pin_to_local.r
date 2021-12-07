c044f17f93242f9d482fa2bc1f953596f0a70ef3
kmasiello <32964481+kmasiello@users.noreply.github.com>
2021-12-06 20:02:57 -0800

    slides updates

---
+++ pin_to_local.r
@@ -0,0 +1,26 @@
+library(pins)
+#local board
+board1 <- board_local()
+board1
+# Pin board <pins_board_folder>
+# Path: '~/Library/Application Support/pins'
+
+#write some pins
+board1 %>% pin_write(mtcars)
+board1 %>% pin_write(faithful)
+board1 %>% pin_write(palmerpenguins::penguins, name = "penguins", versioned = TRUE)
+
+#what do we have?
+board %>% pin_list()
+# [1] "faithful" "mtcars"   "penguins"
+
+#read the pin
+board1 %>% pin_read("mtcars")
+
+#list versions
+board1 %>% pin_versions("penguins")
+# A tibble: 2 × 3
+#   version                created             hash
+#   <chr>                  <dttm>              <chr>
+# 1 20211207T005403Z-84255 2021-12-06 16:54:00 84255
+# 2 20211207T005855Z-84255 2021-12-06 16:58:00 84255
